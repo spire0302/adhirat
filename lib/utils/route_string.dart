b963228130649480e3175217a50fcf73e3ff6876
drashti4444 <drashti.spire4444@gmail.com>
2023-04-28 10:08:54 +0530

    check merge

--- lib/utils/route_string.dart
+++ lib/utils/route_string.dart
@@ -2,6 +2,7 @@
 
 class RouteString {
   static const SPLASH_SCREEN = "/SplashScreen";
+  //
   static const LOGIN_SCREEN = "/LoginScreen";
   static const REGISTER_SCREEN = "/RegisterScreen";
   static const FOEROT_SCREEN = "/ForgotPassword";
